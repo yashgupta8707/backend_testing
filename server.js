307ff3ac8c1f3e700eeefaa56577b8ac8ade2989
yash gupta <yashgupta0096@gmail.com>
2025-03-12 20:43:13 +0530

    Added morgan

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 // ✅ Load environment variables
 dotenv.config();
@@ -10,7 +11,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://frontend-testing-topaz.vercel.app",
+  credentials: true
+}));
+app.use(morgan("dev"));
+
 app.use(bodyParser.json());
 const helmet = require("helmet");
 app.use(helmet());
