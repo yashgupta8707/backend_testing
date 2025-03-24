ee6a984cb29e8a7fdf25de8f5281566c02a4c6c8
yashgupta8707 <yashgupta@spinfluencemedia.com>
2025-03-24 15:19:31 +0530

    Update cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "https://frontend-testing-topaz.vercel.app",
+  origin: ["https://frontend-testing-topaz.vercel.app", "http://localhost:5173", "https://frontend-testing-v2-0.vercel.app"],
   credentials: true
 }));
 app.use(morgan("dev"));
